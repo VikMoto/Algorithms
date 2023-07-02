9f6f95022bb0f83682fc19764d20ad7bec20411d
ig.basalt@gmail.com <ig.basalt@gmail.com>
2023-07-02 18:02:12 +0300

    SelectionSort

---
+++ src/main/java/org/example/sort/SelectionSort.java
@@ -0,0 +1,28 @@
+package org.example.sort;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] array = new int[]{5, 0, -2, 7, 3};
+
+        int minIndex = 0;
+
+        for (int i = 0; i < array.length - 1; i++) {
+            minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[minIndex] > array[j]) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                int temp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
+            }
+        }
+
+        System.out.println("array = " + Arrays.toString(array));
+
+    }
+}
